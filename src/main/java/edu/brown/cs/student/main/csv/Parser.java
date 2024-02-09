6057a3fbf54a304129028406cb41b8e72c2d5c3f
EAuguste25 <eric_auguste@brown.edu>
2024-02-09 16:15:50 -0500

    feat: Parser implementation included

--- src/main/java/edu/brown/cs/student/main/csv/Parser.java
+++ src/main/java/edu/brown/cs/student/main/csv/Parser.java
@@ -1,5 +1,94 @@
 package edu.brown.cs.student.main.csv;
 
-public class Parser {
+import edu.brown.cs.student.main.Exceptions.FactoryFailureException;
+import edu.brown.cs.student.main.Interfaces.CreatorFromRow;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
+public class Parser<T> {
+
+  private CreatorFromRow<T> creator;
+  private Reader reader;
+  static final Pattern regexSplitCSVRow =
+      Pattern.compile(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))");
+
+  /**
+   * This is the constructor for the developer for parsing the csv to the specified data type in the
+   * parameter.
+   *
+   * @param creator the row object
+   * @param reader the input file as a reader object
+   */
+  public Parser(CreatorFromRow<T> creator, Reader reader) {
+    this.creator = creator;
+    this.reader = reader;
+  }
+
+  /**
+   * Constructor for parsing for the user that doesn't need the CreatorFromRow parameter
+   *
+   * @param reader the input file as a reader object
+   */
+  public Parser(Reader reader) {
+    this.reader = reader;
+  }
+
+  /**
+   * This method parses the csv into a list of list strings for the user
+   *
+   * @return a List of Lists of Strings representing the rows in a CSV file
+   * @throws IOException
+   */
+  public List<List<String>> parseUser() throws IOException {
+    List parsedCSVUser = new ArrayList<>();
+
+    try (BufferedReader bufferedReader = new BufferedReader(this.reader)) {
+      String line = bufferedReader.readLine();
+
+      while (line != null) {
+        try {
+          String[] words = regexSplitCSVRow.split(line);
+          List<String> wordList = List.of(words);
+          parsedCSVUser.add(wordList);
+        } catch (NullPointerException npe) {
+          System.out.println("An error occurred, please try again.");
+        }
+        line = bufferedReader.readLine();
+      }
+    } catch (IOException e) {
+      System.out.println("Sorry, an error occurred. Please try again.");
+    }
+    return parsedCSVUser;
+  }
+
+  /**
+   * This method takes advantage of parsing the csv data for the user to provide the data as a list
+   * of strings then changes each row into a type of generic type T
+   *
+   * @return a List of objects representing rows in a CSV file
+   * @throws FactoryFailureException
+   */
+  public List<T> parseDeveloper() throws FactoryFailureException {
+    if (this.creator == null) {
+      throw new FactoryFailureException("CreatorFromRow is not initialized", null);
+    }
+    List<List<String>> userCSV = new ArrayList<>();
+    try {
+      userCSV = this.parseUser();
+    } catch (IOException e) {
+      System.out.println("Error reading CSV file: " + e.getMessage());
+    }
+    List<T> parsedCSVDeveloper = new ArrayList<>();
+
+    for (List<String> row : userCSV) {
+      T newRow = this.creator.create(row);
+      parsedCSVDeveloper.add(newRow);
+    }
+
+    return parsedCSVDeveloper;
+  }
 }
---
+++ src/test/java/edu/brown/cs/student/main/TestParser.java
@@ -0,0 +1,124 @@
+package edu.brown.cs.student.main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import edu.brown.cs.student.main.csv.Parser;
+import edu.brown.cs.student.main.Interfaces.CreatorFromRow;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * Testing class for CSV Parser Read Includes JUnit Testing for edge cases, exceptions and
+ * functionality specifics
+ *
+ * @param <T>
+ */
+public class TestParser<T> {
+
+  private Reader filename;
+  private boolean hasHeader;
+  private CreatorFromRow rowObject;
+
+  /** Testing the case where Read takes in a file that does not exist */
+  @Test
+  public void testFileDoesNotExist() throws NullPointerException {
+    try {
+      this.filename =
+          new FileReader("/Users/ericauguste/Desktop/CS32/csv-EAuguste25/data/stars/nine-star.csv");
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+    }
+    this.hasHeader = true;
+    Parser<T> csvParser = new Parser<>(this.filename);
+    assertThrows(NullPointerException.class, csvParser::parseUser);
+  }
+
+  /** Tests the simple functionality of the parseUser function */
+  @Test
+  public void testParseUser() throws IOException {
+    this.filename =
+        new FileReader("/Users/ericauguste/Desktop/CS32/csv-EAuguste25/data/stars/ten-star.csv");
+
+    Parser<T> csvReader = new Parser<>(this.filename);
+    List<List<String>> result = csvReader.parseUser();
+    assertEquals(11, result.size()); // number of rows
+  }
+
+  /** Tests the functionality of parseUser on a StringReader without a header */
+  @Test
+  public void testReadWithoutHeaderStringReaderUser() throws IOException {
+    String stringCSV = "Eric,21,Boston\nJavier,22,Providence\nAuguste,23,New York";
+    Reader stringReader = new StringReader(stringCSV);
+    Parser<T> csvReader = new Parser<>(stringReader);
+    List<List<String>> result = csvReader.parseUser();
+    assertEquals(3, result.size()); // Three rows
+    assertEquals(List.of("Javier", "22", "Providence"), result.get(1)); // Second data row
+  }
+
+  /** Tests the functionality of parseUser on a StringReader with a header */
+  @Test
+  public void testReadWithStringReader() throws IOException {
+    String stringCSV = "Name,Age,Location\nEric,21,Boston\nJavier,22,Providence";
+    Reader stringReader = new StringReader(stringCSV);
+    Parser<T> csvReader = new Parser(stringReader);
+    List<List<String>> result = csvReader.parseUser();
+    assertEquals(3, result.size());
+    assertEquals(List.of("Name", "Age", "Location"), result.get(0)); // Header row
+  }
+
+  /** Tests the functionality of parseUser on malformed Data */
+  @Test
+  public void testReadMalformedData() throws IOException {
+    this.filename =
+        new FileReader(
+            "/Users/ericauguste/Desktop/CS32/csv-EAuguste25/data/malformed/malformed_signs.csv");
+
+    Parser<T> csvReader = new Parser<>(this.filename);
+    List<List<String>> result = csvReader.parseUser();
+    assertEquals(13, result.size()); // number of rows
+  }
+
+  /** Tests a malformed csv file without a header */
+  @Test
+  public void testReadMalformedWithoutHeader() throws IOException {
+    this.filename =
+        new FileReader(
+            "/Users/ericauguste/Desktop/CS32/csv-EAuguste25/data/withoutHeader/no-header-ten-star.csv");
+
+    Parser<T> csvReader = new Parser<>(this.filename);
+    List<List<String>> result = csvReader.parseUser();
+    assertEquals(10, result.size()); // number of rows
+  }
+
+  /** This tests a csv file that is outside the protected directory */
+  @Test
+  public void testUnprotectedCSVFile() throws FileNotFoundException {
+    try {
+      this.filename =
+          new FileReader(
+              "/Users/ericauguste/Desktop/CS32/csv-EAuguste25/unprotectedData/unprotected.csv");
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+    }
+  }
+  /**
+   * Tests the new csv file for Sprint 2
+   */
+  @Test
+  public void testNewCSV() throws IOException {
+    this.filename =
+        new FileReader(
+            "/Users/ericauguste/Desktop/CS32/Projects/server-eauguste25-ksam417/data/RI City & Town Income from American Community Survey 5-Year Estimates Source_ US Census Bureau, 2017-2021 American Community Survey 5-Year Estimates 2017-2021 - Sheet1.csv");
+
+    Parser<T> csvReader = new Parser<>(this.filename);
+    List<List<String>> result = csvReader.parseUser();
+    assertEquals(41, result.size()); // number of rows
+
+  }
+}
